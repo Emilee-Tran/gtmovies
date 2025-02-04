97e4a10c8d69bdb71690a0cb706e200eca17332a
Emilee-Tran <Emileetran05@gmail.com>
2025-02-04 09:00:55 -0500

    Create and see reviews (Ch 9)
    Users can make and see reviews. Also debugged logout. Edit review function added, but button won't show

--- movies/models.py
+++ movies/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Movie(models.Model):
     id = models.AutoField(primary_key=True)
@@ -9,3 +10,13 @@ class Movie(models.Model):
 
     def __str__(self):
         return str(self.id) + ' - ' + self.name
+
+class Review(models.Model):
+    id = models.AutoField(primary_key=True) # automatically created id
+    comment = models.CharField(max_length=255) # review text, max 255 chars
+    date = models.DateTimeField(auto_now_add=True) # date review made
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE) # movie review is associated w/
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # user that made review
+
+    def __str__(self):
+        return str(self.id) + ' - ' + self.move.name
