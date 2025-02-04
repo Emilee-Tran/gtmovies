ad428b9210686c99c24b5afa7775f95a72051a5e
Emilee-Tran <Emileetran05@gmail.com>
2025-02-03 20:04:21 -0500

    Logout
    Finished Ch8

--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,26 @@ from django.shortcuts import render
 from django.contrib.auth import login as auth_login, authenticate
 from .forms import CustomUserCreationForm, CustomErrorList
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def signup(request):
+    template_data = {}
+    template_data['title'] = 'Sign Up'
+    if request.method == 'GET':
+        template_data['form'] = CustomUserCreationForm()
+        return render(request, 'accounts/signup.html', {'template_data': template_data})
+    elif request.method == 'POST':
+        # use custom forms and error display when trying to sign up
+        form = CustomUserCreationForm(request.POST, error_class=CustomErrorList)
+        if form.is_valid():
+            # Successfully creates user
+            form.save()
+            return redirect('accounts.login')
+        else:
+            template_data['form'] = form
+            return render(request, 'accounts/signup.html', {'template_data': template_data})
+
 def login(request):
     template_data = {}
     template_data['title'] = 'Login'
@@ -26,19 +44,7 @@ def login(request):
             auth_login(request, user)
             return redirect('home.index')
 
-def signup(request):
-    template_data = {}
-    template_data['title'] = 'Sign Up'
-    if request.method == 'GET':
-        template_data['form'] = CustomUserCreationForm()
-        return render(request, 'accounts/signup.html', {'template_data': template_data})
-    elif request.method == 'POST':
-        # use custom forms and error display when trying to sign up
-        form = CustomUserCreationForm(request.POST, error_class=CustomErrorList)
-        if form.is_valid():
-            # Successfully creates user
-            form.save()
-            return redirect('accounts.login')
-        else:
-            template_data['form'] = form
-            return render(request, 'accounts/signup.html', {'template_data': template_data})
+@login_required
+def logout(request):
+    auth_logout(request)
+    return redirect('home.index')
